f363b377b7b9b21985a34f12a5eecf6b3b7a9197
cyf <cyf@cyfs-MacBook-Pro.local>
2025-03-08 11:24:20 +0000

    task1

---
+++ dom-merge-conflict/tasks/buttons-and-counter/src/main.js
@@ -0,0 +1,22 @@
+//increments the number in a node's text
+function increment(node) {
+    let current = node.textContent;
+    node.textContent = Number(current) + 1;
+  }
+  
+  
+    const main = document.createElement("main");
+    main.innerHTML = `
+          <p id="counter" data-testid="counter">0</p>
+          <button id="increment">Increment</button>
+      `;
+    body.appendChild(main);
+  
+    const button = body.querySelector("#increment");
+    const counter = body.querySelector("#counter");
+    button.addEventListener("click", () => {
+      increment(counter);
+    });
+  
+    return body;
+  
